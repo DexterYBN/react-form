b2d36faf41e8189901920e05be543951d7a7b8d3
DexterYBN <yakubov959@mail.ru>
2022-11-27 13:54:34 +0300

    +-

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "../src/App.css"
 
 function App() {
+
+  const [text, setText] = useState("")
+  const [send, setSend] = useState(false)
+  const [error, setError] = useState(false)
+  const [tags, setTags] = useState([])
+
+  const handleText = (e) => { // добавление текста в input
+    setText(e.target.value);
+    setSend(false);
+    setError(false);
+  }
+
+  const handleChangeButton = (e) => { // добавление текста в console
+    e.preventDefault()
+    console.log(text)
+    setText("")
+    setSend(true)
+    setTags([...tags, text])
+  }
+
+  const hundleBlur = () => {
+    if (!text) {
+      setError(true);
+      setSend(false);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main">
+        <div className="forms">
+          <select className="select"> {/* селект для показа списка добавленных дел, показывает при нажатии на стрелку */}
+            {tags.map((item, index) => {
+              return <option key={index}>{item}</option>
+            })}
+          </select>
+          <form onSubmit={handleChangeButton}>
+            <input
+              className="text"
+              type="text"
+              value={text}
+              onChange={handleText}
+              placeholder="пишите"
+              onBlur={hundleBlur}
+            />
+
+            {/* кнопка добавления дела */}
+            <button
+              className="button"
+              type="submit"
+              disabled={!text || text[0] === ""}
+            >
+              добавить
+            </button>
+          </form>
+        </div>
+
+        {/* показывает состояние дел */}
+        <div className="stateTodo">
+          {send && <div className="yesTodo">Дело добавлено</div>}
+          {error && <div className="noTodo"> Дел нет </div>}
+        </div>
+
+        <ul>
+          {tags // Выводит добавленные дела на экран
+            .map((item, index) => {
+              return <li key={index}>{item}</li>
+            })
+            .reverse()}
+        </ul>
+      </div>
+    </>
   );
 }
 
